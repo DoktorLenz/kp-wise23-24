86eb14ffc2ce31f8feb7ec857757011218a72af6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-11-27 00:21:46 +0100

    fix user login

--- quiz/deno/src/user.ts
+++ quiz/deno/src/user.ts
@@ -1,128 +1,138 @@
 import {
-	Input,
-	Secret,
-} from 'https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts';
+  Input,
+  Secret,
+} from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 
-import * as bcrypt from 'https://deno.land/x/bcrypt@v0.4.1/mod.ts';
-import { isLinux } from 'https://deno.land/std@0.196.0/_util/os.ts';
+import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 
 interface IUser {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 }
 
 export class User implements IUser {
-	private _username: string = '';
+  private _username: string = "";
 
-	public get username(): string {
-		return this._username;
-	}
-	private set username(username: string) {
-		this._username = username;
-	}
+  public get username(): string {
+    return this._username;
+  }
+  private set username(username: string) {
+    this._username = username;
+  }
 
-	private _password = '';
+  protected _password = "";
 
-	public set password(unencryptedPassword: string) {
-		this._password = bcrypt.hashSync(unencryptedPassword);
-	}
+  public set password(unencryptedPassword: string) {
+    this._password = bcrypt.hashSync(unencryptedPassword);
+  }
 
-	public get userdata(): IUser {
-		return {
-			username: this.username,
-			password: this._password,
-		};
-	}
+  public get userdata(): IUser {
+    return {
+      username: this.username,
+      password: this._password,
+    };
+  }
 
-	constructor(userdata: IUser) {
-		this.username = userdata.username;
-		this.password = userdata.password;
-	}
+  constructor(data?: IUser) {
+    if (data) {
+      this.username = data.username;
+      this._password = data.password;
+    }
+  }
 
-	private isPasswordValid(password: string): Promise<boolean> {
-		return bcrypt.compare(password, this._password);
-	}
+  private isPasswordValid(password: string): boolean {
+    return bcrypt.compareSync(password, this._password);
+  }
 
-	static async register(): Promise<void> {
-		const users = await getUsers();
+  static deserialize(data: IUser): User {
+    const user = new User();
+    user.username = data.username;
+    user._password = data.password;
+    return user;
+  }
 
-		const username = await Input.prompt({
-			message: 'Choose a username: ',
-			validate: (value) => {
-				if (value.length < 3) {
-					return 'Username must be at least 3 characters long';
-				}
+  static async register(): Promise<void> {
+    const users = await getUsers();
 
-				const isUsernameTaken = users.find((user) =>
-					user._username === value
-				);
-				if (isUsernameTaken) {
-					return 'Username already taken';
-				}
+    const username = await Input.prompt({
+      message: "Choose a username: ",
+      validate: (value) => {
+        if (value.length < 3) {
+          return "Username must be at least 3 characters long";
+        }
 
-				return true;
-			},
-		});
+        const isUsernameTaken = users.find((user) => user._username === value);
+        if (isUsernameTaken) {
+          return "Username already taken";
+        }
 
-		const password = await Secret.prompt({
-			message: 'Set a password: ',
-			validate: (value) =>
-				value.length < 8
-					? 'Password must be at least 8 characters long'
-					: true,
-		});
+        return true;
+      },
+    });
 
-		await Secret.prompt({
-			message: 'Repeate password: ',
-			validate: (value) =>
-				value === password
-					? true
-					: 'Passwords do not match',
-		});
+    const password = await Secret.prompt({
+      message: "Set a password: ",
+      validate: (value) =>
+        value.length < 8 ? "Password must be at least 8 characters long" : true,
+    });
 
-		saveUser(new User({ username, password }));
-	}
+    await Secret.prompt({
+      message: "Repeate password: ",
+      validate: (value) => value === password ? true : "Passwords do not match",
+    });
 
-	static async login(): Promise<User> {
-		const username = await Input.prompt('Username: ');
-		console.log(username);
-		const password = await Secret.prompt('Password: ');
+    saveUser(new User({ username, password }));
+  }
 
-		const users = await getUsers();
+  static async login(): Promise<User> {
+    const username = await Input.prompt("Username: ");
+    console.log(username);
+    const password = await Secret.prompt("Password: ");
 
-		const user = users.find((user) => user._username === username);
-		const isPasswordValid = user &&
-			await user.isPasswordValid(password);
-		if (!isPasswordValid) {
-			throw new Error('Wrong credentials');
-		}
-		return user;
-	}
+    const users = await getUsers();
+
+    const user = users.find((user) => user._username === username);
+    if (!user) {
+      throw new Error("NO USER");
+    }
+    console.log(password);
+    const isPasswordValid = user.isPasswordValid(password);
+    if (!isPasswordValid) {
+      throw new Error("Wrong credentials");
+    }
+    return user;
+  }
 }
 
+const dataDir = `${dir("data")}/quiz`;
+
 export async function getUsers(): Promise<User[]> {
-	const decoder = new TextDecoder();
-	try {
-		const data = await Deno.readFile('./data/users.json');
-		const userdatas = JSON.parse(decoder.decode(data));
-		const users = userdatas.map((user: IUser) => {
-			return new User(user);
-		});
-		return users;
-	} catch (_error) {
-		return [];
-	}
+  const decoder = new TextDecoder();
+  try {
+    const data = await Deno.readFile(`${dataDir}/users.json`);
+    const userdatas = JSON.parse(decoder.decode(data));
+
+    const users = userdatas.map((user: IUser) => {
+      return new User(user);
+    });
+    return users;
+  } catch (_error) {
+    return [];
+  }
 }
 
 export async function saveUser(user: User): Promise<void> {
-	const users = await getUsers();
-	users.push(user);
-	const userdatas: IUser[] = users.map((user) => user.userdata);
-	const encoder = new TextEncoder();
-	await Deno.writeFile(
-		'./data/users.json',
-		encoder.encode(
-			JSON.stringify(userdatas),
-		),
-	);
+  const users = await getUsers();
+  users.push(user);
+  const userdatas: IUser[] = users.map((user) => user.userdata);
+  const encoder = new TextEncoder();
+  await Deno.mkdir(dataDir, { recursive: true });
+  await Deno.writeFile(
+    `${dataDir}/users.json`,
+    encoder.encode(
+      JSON.stringify(userdatas),
+    ),
+    { create: true },
+  );
 }
