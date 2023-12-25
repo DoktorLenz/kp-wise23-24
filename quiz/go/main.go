ec70532fa74336da3eb28d9cd1ff484997b65fb7
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-12-26 00:28:29 +0100

    testing user

--- quiz/go/main.go
+++ quiz/go/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"github.com/DoktorLenz/kp-wise23-24/quiz/go/statemaschine"
+	"github.com/DoktorLenz/kp-wise23-24/quiz/go/user"
 )
 
 func main() {
-	sm := statemaschine.NewStateMachine()
-	sm.Start()
+	// sm := statemaschine.NewStateMachine()
+	// sm.Start()
+	_, _ = user.CreateUser("test", "test")
+	// utils.Prompt("Created user: %+v", u)
+	// check := u.CheckPassword("tesst")
+	// utils.Prompt("Check password: %t", check)
+
+	// u.Save()
 }
