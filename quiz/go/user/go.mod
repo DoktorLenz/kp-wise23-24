9903cb7efcafcfacdc352a1343288443d650b892
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-12-26 00:28:20 +0100

    user in go

--- quiz/go/user/go.mod
+++ quiz/go/user/go.mod
@@ -1,3 +1,8 @@
 module github.com/DoktorLenz/kp-wise23-24/quiz/go/user
 
 go 1.21.3
+
+require (
+	github.com/google/uuid v1.5.0
+	golang.org/x/crypto v0.17.0
+)
--- quiz/go/user/user.go
+++ quiz/go/user/user.go
@@ -1,1 +1,104 @@
 package user
+
+import (
+	"encoding/json"
+
+	"github.com/DoktorLenz/kp-wise23-24/quiz/go/utils"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	// Encoded Password
+	Password string `json:"password"`
+}
+
+type IUser interface {
+	GetID() string
+	GetUsername() string
+	GetEncodedPassword() string
+	CheckPassword(password string) bool
+	Save() error
+}
+
+func CreateUser(username string, password string) (IUser, error) {
+	users, err := GetAllUsers()
+	if err != nil {
+		return nil, err
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{
+		ID:       uuid.New().String(),
+		Username: username,
+		Password: string(passwordHash),
+	}
+
+	users = append(users, user)
+	err = SaveAllUsers(users)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *User) GetID() string {
+	return u.ID
+}
+
+func (u *User) GetUsername() string {
+	return u.Username
+}
+
+func (u *User) GetEncodedPassword() string {
+	return u.Password
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
+func (u *User) Save() error {
+	users, err := GetAllUsers()
+	if err != nil {
+		return err
+	}
+
+	users = append(users, u)
+	return SaveAllUsers(users)
+}
+
+func GetAllUsers() ([]*User, error) {
+
+	var data []*User
+	fileContent, err := utils.ReadFile("users.json")
+	if err != nil {
+		fileContent = []byte("[]")
+	}
+	err = json.Unmarshal(fileContent, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func SaveAllUsers(users []*User) error {
+	b, err := json.Marshal(users)
+	if err != nil {
+		return err
+	}
+	utils.Prompt(string(b), nil)
+	err = utils.WriteFile("users.json", b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
