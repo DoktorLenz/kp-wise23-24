7795f69707932978635f7bfa00bff311ccc6dc04
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-12-26 02:12:40 +0100

    basic question edit func

--- quiz/go/quiz/question.go
+++ quiz/go/quiz/question.go
@@ -1,8 +1,74 @@
 package quiz
 
+import (
+	"errors"
+
+	"github.com/DoktorLenz/kp-wise23-24/quiz/go/utils"
+	"github.com/manifoldco/promptui"
+)
+
 type Question struct {
 	ID          string      `json:"id"`
 	Title       string      `json:"title"`
 	Description string      `json:"description"`
 	Solution    interface{} `json:"solution"`
+}
+
+type CheckAnswerFunc func(answer interface{}) bool
+type AskFunc func() (interface{}, error)
+
+type IQuestion interface {
+	CheckAnswer(CheckAnswerFunc) bool
+	Ask(AskFunc) (interface{}, error)
+	Edit() error
+	GetSolutionText() string
+}
+
+func (q *Question) Edit() error {
+	utils.Clear()
+
+	titlePrompt := promptui.Prompt{
+		Label:   "Please enter the title of the question",
+		Default: q.Title,
+		Validate: func(input string) error {
+			if len(input) == 0 {
+				return errors.New("The title must not be empty")
+			}
+			if len(input) > 100 {
+				return errors.New("The title must not be longer than 100 characters")
+			}
+
+			return nil
+		},
+	}
+
+	descriptionPrompt := promptui.Prompt{
+		Label:   "Please enter the description of the question",
+		Default: q.Description,
+		Validate: func(input string) error {
+			if len(input) > 100 {
+				return errors.New("The description must not be longer than 100 characters")
+			}
+
+			return nil
+		},
+	}
+
+	newTitle, err := titlePrompt.Run()
+	if err != nil {
+		return err
+	}
+	newDescription, err := descriptionPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	q.Title = newTitle
+	q.Description = newDescription
+
+	return nil
+}
+
+func (q *Question) PrintTitle() {
+	utils.Prompt("%s", q.Title)
 }
