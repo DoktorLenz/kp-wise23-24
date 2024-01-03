79f6ed90af8f952c475631e83307eee161362f2b
Laurenz Stinner <laurenz.stinner2@gmail.com>
2024-01-03 02:30:30 +0100

    cleanup main

--- quiz/go/main.go
+++ quiz/go/main.go
@@ -1,27 +1,8 @@
 package main
 
-import (
-	"github.com/DoktorLenz/kp-wise23-24/quiz/go/statemaschine/states"
-	"github.com/DoktorLenz/kp-wise23-24/quiz/go/user"
-)
+import "github.com/DoktorLenz/kp-wise23-24/quiz/go/statemaschine"
 
 func main() {
-	// sm := statemaschine.NewStateMachine()
-	// sm.Start()
-
-	u, _ := user.CheckLogin("admin", "adminadmin")
-	manage := states.NewManageQuizzesState(u)
-	manage.Run()
-
-	// tq := quiz.ToggleQuestion{
-	// 	Question: quiz.Question{
-	// 		Solution: true,
-	// 	},
-	// 	TrueText:  "True",
-	// 	FalseText: "False",
-	// }
-
-	// tq.Edit()
-
-	// utils.Prompt(tq.GetSolutionText())
+	sm := statemaschine.NewStateMachine()
+	sm.Start()
 }
